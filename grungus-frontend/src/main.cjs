a39684ae38f856b171ec3544857fa4aa92cce5fc
fabianmerkl <108922830+fabianmerkl@users.noreply.github.com>
2023-04-09 18:31:17 +0200

    electron

---
+++ grungus-frontend/src/main.cjs
@@ -0,0 +1,33 @@
+// main.js
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  //fucky fucky sus
+  win.loadURL("http://localhost:5173");
+
+  // Open DevTools (optional)
+  win.webContents.openDevTools();
+}
+
+// App ready event
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
